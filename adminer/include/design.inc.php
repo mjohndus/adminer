ded039339e9c4a01de1b787e38a82d8cf52353de
Peter Knut <peter@pematon.com>
2025-02-26 22:40:31 +0100

    Set proper initial scale of the viewport

--- adminer/include/design.inc.php
+++ adminer/include/design.inc.php
@@ -32,7 +32,7 @@ function page_header($title, $error = "", $breadcrumb = [], $title2 = "") {
 <html lang="<?php echo $LANG; ?>" dir="<?php echo lang('ltr'); ?>">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="robots" content="noindex">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $title_page; ?></title>
 <link rel="stylesheet" type="text/css" href="../adminer/static/default.css?<?php echo filemtime("../adminer/static/default.css"); ?>">
 <?php echo script_src("../adminer/static/functions.js?" . filemtime("../adminer/static/functions.js")); ?>
