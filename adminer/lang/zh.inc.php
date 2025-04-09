adminneo-org/adminneo

Update language files, add missing German translation (issue #71)

adminer/lang/zh.inc.php
@@ -19,10 +19,9 @@ $translations = [
 	'AdminNeo does not support accessing a database without a password, <a href="https://www.adminer.org/en/password/"%s>more information</a>.' => 'AdminNeo默认不支持访问没有密码的数据库，<a href="https://www.adminer.org/en/password/"%s>详情见这里</a>.',
 	'Database does not support password.' => '数据库不支持密码。',
 	'Too many unsuccessful logins, try again in %d minute(s).' => '登录失败次数过多，请 %d 分钟后重试。',
-	'Master password expired. <a href="https://www.adminer.org/en/extension/"%s>Implement</a> %s method to make it permanent.' => '主密码已过期。<a href="https://www.adminer.org/en/extension/"%s>请扩展</a> %s 方法让它永久化。',
 	'Language' => '语言',
 	'Invalid CSRF token. Send the form again.' => '无效 CSRF 令牌。请重新发送表单。',
-	'If you did not send this request from AdminNeo then close this page.' => '如果您并没有从AdminNeo发送请求，请关闭此页面。',
+	'If you did not send this request from AdminNeo then close this page.' => '如果您并没有从AAdminerNeo发送请求，请关闭此页面。',
 	'No extension' => '没有扩展',
 	'None of the supported PHP extensions (%s) are available.' => '没有支持的 PHP 扩展可用（%s）。',
 	'Connecting to privileged ports is not allowed.' => '不允许连接到特权端口。',
@@ -93,7 +92,6 @@ $translations = [
 	'Database' => '数据库',
 	'DB' => '数据库',
 	'Use' => '使用',
-	'Select database' => '选择数据库',
 	'Invalid database.' => '无效数据库。',
 	'Database has been dropped.' => '已删除数据库。',
 	'Databases have been dropped.' => '已删除数据库。',
@@ -208,7 +206,6 @@ $translations = [
 	'Alter indexes' => '修改索引',
 	'Add next' => '下一行插入',
 	'Index Type' => '索引类型',
-	// 'Column (length)' => '列（长度）',
 
 	'Foreign keys' => '外键',
 	'Foreign key' => '外键',
@@ -306,8 +303,6 @@ $translations = [
 	'Network' => '网络',
 	'Geometry' => '几何图形',
 	'Relations' => '关联信息',
-
-	// 'Editor' => '编辑器',
 	// date format in Editor: $1 yyyy, $2 yy, $3 mm, $4 m, $5 dd, $6 d
 	'$1-$3-$5' => '$1.$3.$5',
 	// hint for date format - use language equivalents for day, month and year shortcuts
@@ -365,5 +360,7 @@ $translations = [
 	'Check has been created.' => null,
 	'Alter check' => null,
 	'Create check' => null,
+	'length' => null,
 	'Checks' => null,
+	'Invalid permanent login, please login again.' => null,
 ];

mjohndus/adminer

adminer/lang/zh.inc.php
@@ -19,10 +19,9 @@ $translations = [
 	'AdminNeo does not support accessing a database without a password, <a href="https://www.adminer.org/en/password/"%s>more information</a>.' => 'AdminNeo默认不支持访问没有密码的数据库，<a href="https://www.adminer.org/en/password/"%s>详情见这里</a>.',
 	'Database does not support password.' => '数据库不支持密码。',
 	'Too many unsuccessful logins, try again in %d minute(s).' => '登录失败次数过多，请 %d 分钟后重试。',
-	'Master password expired. <a href="https://www.adminer.org/en/extension/"%s>Implement</a> %s method to make it permanent.' => '主密码已过期。<a href="https://www.adminer.org/en/extension/"%s>请扩展</a> %s 方法让它永久化。',
 	'Language' => '语言',
 	'Invalid CSRF token. Send the form again.' => '无效 CSRF 令牌。请重新发送表单。',
-	'If you did not send this request from AdminNeo then close this page.' => '如果您并没有从AdminNeo发送请求，请关闭此页面。',
+	'If you did not send this request from AdminNeo then close this page.' => '如果您并没有从AAdminNeo发送请求，请关闭此页面。',
 	'No extension' => '没有扩展',
 	'None of the supported PHP extensions (%s) are available.' => '没有支持的 PHP 扩展可用（%s）。',
 	'Connecting to privileged ports is not allowed.' => '不允许连接到特权端口。',
@@ -93,7 +92,6 @@ $translations = [
 	'Database' => '数据库',
 	'DB' => '数据库',
 	'Use' => '使用',
-	'Select database' => '选择数据库',
 	'Invalid database.' => '无效数据库。',
 	'Database has been dropped.' => '已删除数据库。',
 	'Databases have been dropped.' => '已删除数据库。',
@@ -208,7 +206,6 @@ $translations = [
 	'Alter indexes' => '修改索引',
 	'Add next' => '下一行插入',
 	'Index Type' => '索引类型',
-	// 'Column (length)' => '列（长度）',
 
 	'Foreign keys' => '外键',
 	'Foreign key' => '外键',
@@ -306,8 +303,6 @@ $translations = [
 	'Network' => '网络',
 	'Geometry' => '几何图形',
 	'Relations' => '关联信息',
-
-	// 'Editor' => '编辑器',
 	// date format in Editor: $1 yyyy, $2 yy, $3 mm, $4 m, $5 dd, $6 d
 	'$1-$3-$5' => '$1.$3.$5',
 	// hint for date format - use language equivalents for day, month and year shortcuts
@@ -365,5 +360,7 @@ $translations = [
 	'Check has been created.' => null,
 	'Alter check' => null,
 	'Create check' => null,
+	'length' => null,
 	'Checks' => null,
+	'Invalid permanent login, please login again.' => null,
 ];