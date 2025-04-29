1b4b866e17289e0476c9cb795bb9d177a73a8d05
Peter Knut <peter@pematon.com>
2025-04-29 21:55:38 +0200

    JsonPreviewPlugin: Fix opening the right JSON preview in table rows loaded by AJAX

--- plugins/JsonPreviewPlugin.php
+++ plugins/JsonPreviewPlugin.php
@@ -28,6 +28,9 @@ class JsonPreviewPlugin
 	/** @var int */
 	private $maxTextLength;
 
+	/** @var string */
+	private $linkIdBase;
+
 	/** @var int */
 	private $counter = 1;
 
@@ -43,6 +46,8 @@ class JsonPreviewPlugin
 		$this->inEdit = $inEdit;
 		$this->maxLevel = $maxLevel;
 		$this->maxTextLength = $maxTextLength;
+
+		$this->linkIdBase = (string)microtime(true);
 	}
 
 	/**
@@ -108,7 +113,7 @@ class JsonPreviewPlugin
 			return null;
 		}
 
-		return "<a class='toggle jsonly' href='#json-code-$this->counter' title='JSON' data-value='" . h($val) . "'>" . icon_chevron_right() . "</a>" .
+		return "<a class='toggle jsonly' href='#json-code-$this->linkIdBase-$this->counter' title='JSON' data-value='" . h($val) . "'>" . icon_chevron_right() . "</a>" .
 			" <code class='jush-js'>$val</code>" .
 			$this->buildTable($json, 1, $this->counter++);
 
@@ -125,7 +130,7 @@ class JsonPreviewPlugin
 			return null;
 		}
 
-		return "<div class='jsonly'><a class='toggle' href='#json-code-$this->counter'>JSON" . icon_chevron_down() . "</a></div>" .
+		return "<div class='jsonly'><a class='toggle' href='#json-code-$this->linkIdBase-$this->counter'>JSON" . icon_chevron_down() . "</a></div>" .
 			$this->buildTable($json, 1, $this->counter++) .
 			"<textarea $attrs cols='50' rows='12' class='jush-js'>" . h($value) . "</textarea>";
 
@@ -151,9 +156,9 @@ class JsonPreviewPlugin
 
 	}
 
-	private function buildTable(array $json, int $level = 1, int $id = 0): string
+	private function buildTable(array $json, int $level = 1, int $counter = 0): string
 	{
-		$value = "<table class='json hidden'" . ($id && $level == 1 ? " id='json-code-$id'" : "") . ">";
+		$value = "<table class='json hidden'" . ($counter && $level == 1 ? " id='json-code-$this->linkIdBase-$counter'" : "") . ">";
 
 		foreach ($json as $key => $val) {
 			$value .= "<tr><th><code>" . h($key) . "</code>";
